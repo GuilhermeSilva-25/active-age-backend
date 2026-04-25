a4d2ab4f2504f99c881cf796621250949d22e87c
Guilhereme Silva <guime.silva333@gmail.com>
2026-04-25 15:14:28 -0300

    feat: inicializa projeto spring boot com dependencias base

---
+++ src/test/java/com/activeage/api/ActiveAgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activeage.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveAgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
