436b1e0428bcd35079fa92b1e248d27a92045403
GuilhermeAraujoCoelho <guilhermearaujo4589@gmail.com>
2026-09-22 00:20:57 -0300

    fix: altera data dos agendamentos

--- src/main/java/com/activeage/api/config/DataSeeder.java
+++ src/main/java/com/activeage/api/config/DataSeeder.java
@@ -104,7 +104,7 @@ public class DataSeeder {
 
                 usuarioRepository.saveAll(lendasTech);
 
-                LocalDateTime dataBase = LocalDateTime.of(2026, 9, 1, 8, 0).truncatedTo(ChronoUnit.MINUTES);
+                LocalDateTime dataBase = LocalDateTime.of(2026, 9, 22, 8, 0).truncatedTo(ChronoUnit.MINUTES);
                 List<Agendamento> todosAgendamentos = new ArrayList<>();
 
                 for (Usuario medico : lendasTech) {
